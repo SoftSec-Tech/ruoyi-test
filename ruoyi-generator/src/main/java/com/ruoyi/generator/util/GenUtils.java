11986d39bc53424202b5268263f6534cae753332
shixiao <zhangshixiao@softsec-tech.cn>
2026-05-27 15:08:22 +0800

    add toTitleCase utility method

--- ruoyi-generator/src/main/java/com/ruoyi/generator/util/GenUtils.java
+++ ruoyi-generator/src/main/java/com/ruoyi/generator/util/GenUtils.java
@@ -390,4 +390,10 @@ public class GenUtils
         }
         return -1;
     }
+
+    public static String toTitleCase(String str)
+    {
+        if (str == null || str.isEmpty()) return str;
+        return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
 }
